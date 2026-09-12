ada3c9ec03457e1b4f82f25539095970c53d94b4
Jason <jasonugochukwu9@gmail.com>
2026-09-12 23:47:16 +0100

    Add input validation for electricity readings

---
+++ Electricity analyzer.py
@@ -0,0 +1,21 @@
+while True:
+    days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+    readings = input("Enter the readings for 7 days: ")
+    readings_list = readings.split()
+    if len(readings_list) != 7:
+       continue
+    else:
+        valid = True
+        integer_readings = []
+        for reading in readings_list:
+            try:
+                new_value = int(reading)
+                integer_readings.append(new_value)
+            except ValueError:
+                valid = False
+        if valid == False:
+            continue
+        else:
+
+            print(integer_readings)
+        break
